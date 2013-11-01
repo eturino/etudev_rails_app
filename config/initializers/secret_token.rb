a5ae6df3dbf11b5f42e60791b3f4f146652ec939
Eduardo Turiño <eturino@gmail.com>
2013-11-01 18:30:16 +0100

    first commit, new Rails 4 app without test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EtudevApp::Application.config.secret_key_base = '2ec528b0baee4c23239f1b0598f2ce464639ef65af648c2a43dcc77ebb523259ac8bec455fc8ab0d1e6bfd946f4f75db318263ea97bbaf578387b7eab1dbc3d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EtudevApp::Application.config.session_store :cookie_store, key: '_etudev_app_session'
